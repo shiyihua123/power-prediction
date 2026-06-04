cf6b2394700ad89f3b733747105cd18a67926057
shiyihua123 <1960622591@qq.com>
2026-06-04 14:06:38 +0000

    找出lightgbm滚动预测一个小bug

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,4 +1,11 @@
-import torch
-print(torch.cuda.is_available())
-print(torch.version.cuda)
-print(torch.version.hip)
+path = "/home/syh/workplace/PythonProject/power-prediction/outputs/NHITS/cv_results_20260604.csv"
+
+
+
+
+import pandas as pd
+df = pd.read_csv(path)
+print(df)
+
+# 打印出begin_utc列所有的unique值
+print(df['begin_utc'].unique())
